8bd369a12c0eb1784f770ddb24affe2a34cf9b69
karpo99 <karpo99old@gmail.com>
2025-11-01 13:08:48 +0200

    initial commit

---
+++ src/main/java/org/example/currencyapi/CurrencyApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.currencyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyApiApplication.class, args);
+    }
+
+}
