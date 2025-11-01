3eb2f31bba011197110bfc90cc326be3ed8205dd
karpo99 <karpo99old@gmail.com>
2025-11-01 13:35:03 +0200

    add WebClient configuration and loging

---
+++ src/main/java/org/example/currencyapi/config/WebClientConfig.java
@@ -0,0 +1,70 @@
+package org.example.currencyapi.config;
+
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
+
+@Slf4j
+@Configuration
+public class WebClientConfig {
+
+    private static final String API_REQUEST_HEADER = "X-API-KEY";
+
+    @Value("${mock.api.base-url}")
+    private String baseUrl;
+
+    @Value("${mock.api.secret.key}")
+    private String apiSecretKey;
+
+    @Value("${api.timeout}")
+    private int timeout;
+
+    @Bean
+    public WebClient buildWebClient() {
+        //Timeout configuration
+        HttpClient httpClient = HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, timeout)
+                .responseTimeout(Duration.ofMillis(timeout))
+                .doOnConnected(connection ->
+                        connection.addHandlerLast(new ReadTimeoutHandler(timeout, TimeUnit.MILLISECONDS))
+                                .addHandlerLast(new WriteTimeoutHandler(timeout, TimeUnit.MILLISECONDS)));
+
+        return WebClient.builder()
+                .baseUrl(baseUrl)
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader(API_REQUEST_HEADER, apiSecretKey)
+                .filter(logRequest())
+                .filter(logResponse())
+                .build();
+
+    }
+
+    private ExchangeFilterFunction logRequest() {
+        return ExchangeFilterFunction.ofRequestProcessor(request -> {
+            log.info("Sending request {} {}", request.method(), request.url());
+            return Mono.just(request);
+        });
+    }
+
+    private ExchangeFilterFunction logResponse() {
+        return ExchangeFilterFunction.ofResponseProcessor(response -> {
+            log.info("Received response with status: {}", response.statusCode());
+            return Mono.just(response);
+        });
+    }
+}
