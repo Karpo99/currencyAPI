53c141e8f3a1c667bf9f715654fe08b465f3577e
karpo99 <karpo99old@gmail.com>
2025-11-02 12:17:23 +0200

    add liquibase

--- schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS currency_rates (
-                                              id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                                              currency VARCHAR(10) NOT NULL,
-                                              rate DECIMAL(19, 6) NOT NULL,
-                                              currency_type VARCHAR(20) NOT NULL,
-                                              created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-);
